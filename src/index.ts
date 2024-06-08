e835707341e9d7ffb7c60b5f5d07af352d316362
fengmk2 <suqian.yf@antgroup.com>
2024-06-08 19:24:35 +0800

    feat: support cjs and esm both
    BREAKING CHANGE: drop Node.js < 18 support
    
    https://github.com/eggjs/egg/issues/5257

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import mimeTypes from 'mime-types';
+import LRU from 'ylru';
+
+let typeLRUCache: LRU;
+
+export function getType(type: string): string | false {
+  if (!typeLRUCache) {
+    typeLRUCache = new LRU(100);
+  }
+  let mimeType = typeLRUCache.get<string | false>(type);
+  if (mimeType === undefined) {
+    mimeType = mimeTypes.contentType(type);
+    typeLRUCache.set(type, mimeType);
+  }
+  return mimeType;
+}
+
+export default getType;
